5af7c3ffd1a5c0fc08525311775d8189b92e4692
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-22 10:59:54 +0530

    const added

---
+++ unet_in_autoencoder/const.py
@@ -0,0 +1,9 @@
+WIDTH, HEIGHT = 256, 256
+BATCH_SIZE = 16
+lr = 1e-04
+epochs = 100
+image_size = [256,256]
+
+
+model_path = "/kaggle/working/unet.keras"
+csv_path = "/kaggle/working/csvlogger.csv"
